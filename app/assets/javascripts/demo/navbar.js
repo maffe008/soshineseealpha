740335d4e1de22999dc2c5674dce115ef5e980bb
Maffee <maffe008@hotmail.com>
2016-10-14 22:04:22 +0800

    DEMO MAP
    Baidu API

---
+++ app/assets/javascripts/demo/navbar.js
@@ -0,0 +1,9 @@
+/**
+ * Created by maffee on 16/10/14.
+ */
+$(function (){
+    $('#navbar li').mouseover(function (){
+        $('.active').removeClass('active');
+        $(this).addClass("active");
+    });
+})
