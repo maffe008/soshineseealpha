282dec1b6cfe0777d237cbaa3b454f28a69647d2
Maffee <maffe008@hotmail.com>
2016-10-31 15:09:41 +0800

    DEMO TOOL
    Demo Tools

--- app/assets/javascripts/demo/Tools.js
+++ app/assets/javascripts/demo/Tools.js
@@ -146,14 +146,56 @@ function btn_spec(){
 
 }
 
-function btn_ruler(){
-
-
+function btn_xy(){
+    map.addEventListener("click",function(e){
+        alert(e.point.lng + "," + e.point.lat);
+    });
 }
 
 function btn_draw(){
+    var styleOptions = {
+        strokeColor:"red",    //边线颜色。
+        fillColor:"white",      //填充颜色。当参数为空时，圆形将没有填充效果。
+        strokeWeight: 3,       //边线的宽度，以像素为单位。
+        strokeOpacity: 1,	   //边线透明度，取值范围0 - 1。
+        fillOpacity: 1,      //填充的透明度，取值范围0 - 1。
+        strokeStyle: 'solid' //边线的样式，solid或dashed。
+    };
+    //实例化鼠标绘制工具
+    var drawingManager = new BMapLib.DrawingManager(map, {
+        isOpen: false, //是否开启绘制模式
+        enableDrawingTool: true, //是否显示工具栏
+        drawingToolOptions: {
+            anchor: BMAP_ANCHOR_TOP_RIGHT, //位置
+            offset: new BMap.Size(5, 5), //偏离值
+        },
+        enableCalculate: true,
+        circleOptions: styleOptions, //圆的样式
+        polylineOptions: styleOptions, //线的样式
+        polygonOptions: styleOptions, //多边形的样式
+        rectangleOptions: styleOptions //矩形的样式
+    });
+    drawingManager.addEventListener("overlaycomplete", function(e) {
+        var mode = e.drawingMode;
+        var result = e.calculate;
+        if(mode == "polyline"){
+            if(result<1000){
+                alert(result + "米");
+            }
+            else{
+                alert(result/1000 + "千米")
+            }
+        }
+        else{
+            if(result<1000000){
+                alert(result + "平方米");
+            }
+            else{
+                alert(result/1000000 + "平方千米")
+            }
+        }
 
-
+    });
 }
 
 function btn_go(){
@@ -163,6 +205,7 @@ function btn_go(){
 }
 
 function btn_clear(){
+
     map.clearOverlays();
     isGroundOverlay = false;
 }
