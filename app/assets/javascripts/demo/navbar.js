973b268b201609e93ebebb5ad5040a8d9d152975
Maffee <maffe008@hotmail.com>
2016-11-14 16:50:36 +0800

    0.2.0|FinishUP&Index&TaskUpdate

--- app/assets/javascripts/demo/navbar.js
+++ app/assets/javascripts/demo/navbar.js
@@ -1,9 +1,11 @@
 /**
  * Created by maffee on 16/10/14.
  */
+/*
 $(function (){
     $('#navbar li').mouseover(function (){
         $('.active').removeClass('active');
         $(this).addClass("active");
     });
 })
+*/
