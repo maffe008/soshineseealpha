8d9ddc817b73260280b2d623a99087c9901757de
Maffee <maffe008@hotmail.com>
2016-11-19 20:41:32 +0800

    0.2.0|hdmapinfo

--- app/assets/javascripts/demo/Demo_Boluo_OutlineOvelay.js
+++ app/assets/javascripts/demo/Demo_Boluo_OutlineOvelay.js
@@ -179,36 +179,36 @@ function add_OutlineOverlay_Demo_Block_Boluo_B_01(){
     var B_BP12 = new BMap.Point(114.204449, 23.125473);
     //Boluo_BlockB01
     var B_BB01 = [B_BP01,B_BP02,B_BP03,B_BP04,B_BP07,B_BP08,B_BP09,B_BP12];
-    map.removeOverlay(outlineOverlay)
+    map.removeOverlay(outlineOverlay);
     outlineOverlay = new BMap.Polygon(B_BB01, OutlinepolygonOptions);
     map.addOverlay(outlineOverlay);
 }
-
 function add_OutlineOverlay_Demo_Block_Boluo_B_02(){
-    //Boluo_BlockPoint01 ...
-    var B_BP04 = new BMap.Point(114.206336, 23.126067);
-    var B_BP05 = new BMap.Point(114.206641, 23.124908);
-    var B_BP06 = new BMap.Point(114.206165, 23.124721);
-    var B_BP07 = new BMap.Point(114.205694, 23.125714);
-    //Boluo_BlockB02
-    var B_BB02 = [B_BP04,B_BP05,B_BP06,B_BP07];
-    map.removeOverlay(outlineOverlay)
-    outlineOverlay = new BMap.Polygon(B_BB02, OutlinepolygonOptions);
-    map.addOverlay(outlineOverlay);
-}
-
-function add_OutlineOverlay_Demo_Block_Boluo_B_03(){
     //Boluo_BlockPoint01 ...
     var B_BP09 = new BMap.Point(114.205213, 23.125532);
     var B_BP10 = new BMap.Point(114.205159, 23.124975);
     var B_BP11 = new BMap.Point(114.204355, 23.124688);
     var B_BP12 = new BMap.Point(114.204449, 23.125473);
     //Boluo_BlockB01
-    var B_BB03 = [B_BP09,B_BP10,B_BP11,B_BP12];
-    map.removeOverlay(outlineOverlay)
+    var B_BB02 = [B_BP09,B_BP10,B_BP11,B_BP12];
+    map.removeOverlay(outlineOverlay);
+    outlineOverlay = new BMap.Polygon(B_BB02, OutlinepolygonOptions);
+    map.addOverlay(outlineOverlay);
+}
+function add_OutlineOverlay_Demo_Block_Boluo_B_03(){
+    //Boluo_BlockPoint01 ...
+    var B_BP04 = new BMap.Point(114.206336, 23.126067);
+    var B_BP05 = new BMap.Point(114.206641, 23.124908);
+    var B_BP06 = new BMap.Point(114.206165, 23.124721);
+    var B_BP07 = new BMap.Point(114.205694, 23.125714);
+    //Boluo_BlockB02
+    var B_BB03 = [B_BP04,B_BP05,B_BP06,B_BP07];
+    map.removeOverlay(outlineOverlay);
     outlineOverlay = new BMap.Polygon(B_BB03, OutlinepolygonOptions);
     map.addOverlay(outlineOverlay);
 }
+
+
 
 
 function add_OutlineOverlay_Demo_Block_Boluo_C_01(){
